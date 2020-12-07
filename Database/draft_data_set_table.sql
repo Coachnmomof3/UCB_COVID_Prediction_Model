9fdba4980a9a57a7298a8aa286a87bed84d54057
jamesdemott <jamesandersondemott@gmail.com>
2020-12-06 19:21:41 -0800

    actually ready for submission

---
+++ Database/draft_data_set_table.sql
@@ -0,0 +1,9 @@
+draft_data_set_table.sql
+
+CREATE TABLE All_Img_Data (
+    condition varchar, 
+    file_name  varchar, 
+    file_link varchar, 
+    test_train boolean, 
+    ML_Prediction INTEGER, 
+)
