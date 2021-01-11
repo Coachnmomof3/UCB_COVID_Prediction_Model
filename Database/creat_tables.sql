46fc2dc2a676e6bb2e62ef32b41a8484df6cff5a
jamesdemott <jamesandersondemott@gmail.com>
2021-01-10 17:58:20 -0800

    adding table for image prediction results

--- Database/creat_tables.sql
+++ Database/creat_tables.sql
@@ -10,13 +10,22 @@ CREATE TABLE ML_model_history(
 CREATE TABLE original_image_data (
     file_name  varchar, 
     condition_actual varchar, 
-    test_or_train varchar, 
-)
+    test_or_train varchar 
+);
 
 CREATE Table original_image_record (
   model_no varchar, 
   Epochs int, 
   loss int, 
   val_loss int, 
-  val_categorical_accuracy int, 
-)
+  val_categorical_accuracy int
+); 
+
+
+CREATE Table img_results (
+model_no varchar, 
+file_name varchar, 
+prdecited_likelihood_covid numeric,
+prdecited_likelihood_pneumonia numeric,
+prdecited_likelihood_no_finding numeric
+	); 
