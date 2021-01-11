192b0735bcb387346556e7cbc14e8e0f25ca23b4
jamesdemott <jamesandersondemott@gmail.com>
2021-01-10 18:16:17 -0800

    removing accidental dupe table

--- Database/creat_tables.sql
+++ Database/creat_tables.sql
@@ -1,32 +1,23 @@
 CREATE TABLE ML_model_history(
     model_no varchar,
     Basic_Model_Name varchar,
-    Epochs intloss numeric,
+    Epochs int
+    loss numeric,
     categorical_accuracy numeric, 
     val_loss numeric, 
-    val_vategorical_accuracy numeric 
+    val_categorical_accuracy numeric 
 );
 
 CREATE TABLE original_image_data (
     file_name  varchar, 
-    condition_actual varchar, 
-    test_or_train varchar 
+    condition_actual varchar
 );
-
-CREATE Table original_image_record (
-  model_no varchar, 
-  Epochs int, 
-  loss int, 
-  val_loss int, 
-  val_accuracy int
-); 
 
 
 CREATE Table img_results (
-model_no varchar, 
-file_name varchar, 
-prdecited_likelihood_covid numeric,
-prdecited_likelihood_pneumonia numeric,
-prdecited_likelihood_no_finding numeric
+  model_no varchar, 
+  file_name varchar, 
+  prdecited_likelihood_covid numeric,
+  prdecited_likelihood_pneumonia numeric,
+  prdecited_likelihood_no_finding numeric
 	); 
-
