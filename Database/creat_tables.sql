7b79acfd1c33e3d495cf0c982270406d5cfd4c1c
jamesdemott <jamesandersondemott@gmail.com>
2021-01-10 18:33:58 -0800

    collaprsing sql files to one file

--- Database/creat_tables.sql
+++ Database/creat_tables.sql
@@ -14,10 +14,20 @@ CREATE TABLE original_image_data (
 );
 
 
-CREATE Table img_results (
-  model_no varchar, 
+CREATE Table final_img_results (
   file_name varchar, 
   prdecited_likelihood_covid numeric,
   prdecited_likelihood_pneumonia numeric,
   prdecited_likelihood_no_finding numeric
 	); 
+
+ 
+CREATE TABLE final_img_results_joined_with_actual( 
+ final_img_results.file_name VARCHAR, 
+ final_img_results.prdecited_likelihood_covid numeric,
+ final_img_results.prdecited_likelihood_pneumonia numeric, 
+ final_img_results.likelihood_no_finding numeric, 
+ original_image_data.condition_actual VARCHAR 
+FROM final_img_results 
+LEFT JOIN final_img_results ON final_img_results.file_name = original_image_data.file_name;
+ ); 
--- Database/join_syntax.sql
+++ Database/join_syntax.sql
@@ -2,14 +2,14 @@
 in the original image and then to join with the predictions using the image name as the primary key*/ 
 
  CREATE TABLE ouptut_joined_original_actual( 
- ML_output_table.model_no VARCHAR, 
- ML_output_table.file_name VARCHAR, 
- ML_output_table.likelihood_covid int,
- ML_output_table.likelihood_pneumonia int, 
- ML_output_table.likelihood_no_finding int, 
-original_image_record.condition_actual int 
-FROM ML_output_table 
-LEFT JOIN original_image_record ON ML_output_table.model_no=original_image_record.file_name;
+ ML_model_history.model_no VARCHAR, 
+ ML_model_history.file_name VARCHAR, 
+ ML_model_history.likelihood_covid numeric,
+ ML_model_history.likelihood_pneumonia numeric, 
+ ML_model_history.likelihood_no_finding numeric, 
+original_image_data.condition_actual VARCHAR 
+FROM ML_model_history 
+LEFT JOIN img_results ON ML_model_history.file_name = original_image_data.file_name;
  )
 
 
